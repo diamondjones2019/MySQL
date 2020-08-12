6c1a89cc3e8315a3e80ca6890668d7600a9ea575
Diamond Jones <56932815+diamondjones2019@users.noreply.github.com>
2020-08-12 18:44:06 -0400

    Add files via upload

---
+++ Song data.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM test.music
+-- Date: 2020-08-07 10:55
+*/
+INSERT INTO `` (`Genre`,`Artist`,`Song`,`Chart`) VALUES ('Hip-Hop','Missy Elliot','Loose Control',2);
+INSERT INTO `` (`Genre`,`Artist`,`Song`,`Chart`) VALUES ('Pop','Adele','Someone Like You',10);
+INSERT INTO `` (`Genre`,`Artist`,`Song`,`Chart`) VALUES ('R&B','Ne-Yo','Miss Independent',5);
+INSERT INTO `` (`Genre`,`Artist`,`Song`,`Chart`) VALUES ('Rap','Megan The Stallion','Savage',9);
