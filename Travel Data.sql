b8c0b5bb014e87781c8d688a4e991c7026df73f5
Diamond Jones <56932815+diamondjones2019@users.noreply.github.com>
2020-08-12 18:44:30 -0400

    Add files via upload

---
+++ Travel Data.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM test.travel
+-- Date: 2020-08-07 11:21
+*/
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (1,'New York','NY');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (2,'Los Angeles','CA');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (3,'Chicago','IL');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (4,'Houston','TX');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (5,'Philadelphia','PA');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (6,'Phonenix','AZ');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (7,'San Antonio','TX');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (8,'Atlantic City','NJ');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (9,'Indianapolis','IN');
+INSERT INTO `` (`Popular`,`City`,`State`) VALUES (10,'Jackson','FL');
